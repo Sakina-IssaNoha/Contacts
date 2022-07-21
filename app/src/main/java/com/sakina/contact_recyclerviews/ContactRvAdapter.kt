e54e6384f88a1cf72e5674265a05f8b44d9b7a33
Sakina-IssaNoha <sakinnanoha@gmail.com>
2022-07-21 19:55:41 +0300

    circle images

--- app/src/main/java/com/sakina/contact_recyclerviews/ContactRvAdapter.kt
+++ app/src/main/java/com/sakina/contact_recyclerviews/ContactRvAdapter.kt
@@ -1,11 +1,9 @@
 package com.sakina.contact_recyclerviews
 
+import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.sakina.contact_recyclerviews.databinding.ContactListItemBinding
 import com.squareup.picasso.Picasso
@@ -27,6 +25,17 @@ class ContactRvAdapter(var contactList: List<Contact>):
         holder.binding.tvAddress.text=CurrentContact.address
         Picasso.get().load(CurrentContact.image).resize(200,200).centerCrop()
             .placeholder(R.drawable.ic_baseline_person_24).into(holder.binding.imgContact)
+
+        val context = holder.itemView.context
+        holder.binding.imgContact.setOnClickListener {
+            Toast.makeText(context,"You have clicked the image",Toast.LENGTH_SHORT).show()
+        }
+        holder.binding.cvContact.setOnClickListener{
+            val intent = Intent(context,ViewContactActivity::class.java)
+            intent.putExtra("Name",CurrentContact.name)
+            intent.putExtra("ADDRESS",CurrentContact.address)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
